2732b46e1818123d5ab23f318c5b476ac21ba8ae
Vladimir Makhanov <makhanov.vladimir@gmail.com>
2022-07-26 16:24:56 +0500

    fix tests

--- eventstream/tests/test_eventstream.py
+++ eventstream/tests/test_eventstream.py
@@ -1,3 +1,11 @@
+from typing import TYPE_CHECKING
+
+import pandas as pd
+import pytest
+
+if TYPE_CHECKING:
+    from pandas._typing import DataFrame
+
 from eventstream import Eventstream
 from eventstream.tools import generate_clickstream
 
@@ -7,3 +15,25 @@ class TestCreateEventStream:
         cs = generate_clickstream(rows=10)
         es = Eventstream(dataset=cs, source_schema=list(cs.keys()), schema=["event_timestamp", "user_id", "event"])
         assert len(es) == 10
+
+    def test_wrong_dataset_type(self) -> None:
+        cs: "DataFrame" = pd.DataFrame({"event_name": ["aaa", "bbb"]})
+        with pytest.raises(AssertionError):
+            Eventstream(dataset=cs, source_schema=list(cs.keys()), schema=["event_timestamp", "user_id", "event"])
+
+    def test_wrong_source_schema(self) -> None:
+        cs = generate_clickstream(rows=10)
+        with pytest.raises(AssertionError):
+            Eventstream(dataset=cs, source_schema=["user_id"], schema=["event_timestamp", "user_id", "event"])
+
+    def test_wrong_schema(self) -> None:
+        cs = generate_clickstream(rows=10)
+        with pytest.raises(AssertionError):
+            Eventstream(
+                dataset=cs,
+                source_schema=list(cs.keys()),
+                schema=[
+                    "event_timestamp",
+                    "user_id",
+                ],
+            )
